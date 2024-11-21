3a446b4971ed46ad36edc023d7a3932030835023
BrayanQuinonesGuerrero <brayanquinonesguerrero@gmail.com>
2024-11-21 12:32:34 -0500

    adding the contact page

--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'home/contact.html'
